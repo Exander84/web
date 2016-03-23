0bbdca2bec6cc60ca4c567d943cae9a6aba478be
exander84 <exander84@gmail.com>
2016-03-23 13:29:35 +0300

    update

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 django-admin startproject ask
+chmod 777 ask/manage.py
 ask/manage.py startapp qa
-cp ../views.py qa/
-cp ../urls.py qa/
+cp views.py ask/qa/
+cp urls.py ask/qa/
